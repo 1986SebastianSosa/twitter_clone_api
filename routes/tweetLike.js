2c8cd30cd8fe4c02a2d59e1e824e52ca88256100
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-09-17 19:06:15 -0300

    Tweetlike dontroller JWT verification

--- routes/tweetLike.js
+++ routes/tweetLike.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { postLike, getLikes } = require("../controllers/tweetLikes");
+const verifyJWT = require("../middlewares/verifyJWT");
+
+router.use(verifyJWT);
 
 router.get("/:id", getLikes);
 router.post("/:id", postLike);
