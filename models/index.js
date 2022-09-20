25c2756fc9d3a31e40a99a30aefd03e3c1ac442f
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-09-20 16:08:45 -0300

    Update index

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const dbInitialSetup = require("./seeders/dbInitialSetup");
 
 const mongooseConnection = async () => {
   await mongoose
-    .connect("mongodb://127.0.0.1/twitter_clone", {
+    .connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
