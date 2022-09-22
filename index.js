4058e5467407b7eff9edb9f38f738ac68eadf494
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-09-22 07:46:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const dbConnect = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ credentials: true, optionsSuccessStatus: 200 }));
 app.use(cookieParser());
 
 app.use("/user", userRoutes);
