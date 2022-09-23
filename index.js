744584f71ee8f1037245d00d902721d37c869d39
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-09-23 07:44:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const dbConnect = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "https://twitter-clone-front.vercel.app/" }));
+app.use(cors({ origin: "*" }));
 app.use(cookieParser());
 
 app.use("/user", userRoutes);
