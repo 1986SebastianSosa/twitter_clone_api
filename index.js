21edbac11d5e82973031d114bd9b1ea94bbc6a5f
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-09-25 11:05:09 -0300

    Revert "Update index.js"
    This reverts commit 4058e5467407b7eff9edb9f38f738ac68eadf494.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const dbConnect = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, optionsSuccessStatus: 200 }));
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/user", userRoutes);
