5ae3593a50af24b2e50125c081f3c983bb2cd599
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2022-09-25 19:34:24 -0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,10 +3,13 @@ const dbInitialSetup = require("./seeders/dbInitialSetup");
 
 const mongooseConnection = async () => {
   await mongoose
-    .connect("mongodb://127.0.0.1/twitter_clone", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      "mongodb+srv://SebastianSosa:s41407532@cluster0.coqn4la.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => {
       console.log("There was an error: ", err);
