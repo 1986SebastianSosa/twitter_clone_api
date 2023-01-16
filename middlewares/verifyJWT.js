a3bb2a8b7fda174d6dd70a639a7d1b6c72d70a2f
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2023-01-16 13:25:19 -0300

    verify JWT

--- middlewares/verifyJWT.js
+++ middlewares/verifyJWT.js
@@ -5,14 +5,14 @@ require("dotenv").config();
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
   if (!authHeader?.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ msg: "Unauthorized" });
   }
   const token = authHeader.split(" ")[1];
 
   jwt.verify(token, process.env.JWT_ACCESS_SECRET, async (err, decoded) => {
-    if (err) return res.status(403).json({ message: "Forbidden" });
+    if (err) return res.status(403).json({ msg: "Forbidden" });
     req.userId = decoded.id;
-    // console.log("token verified");
+    console.log("token verified");
     next();
   });
 };
