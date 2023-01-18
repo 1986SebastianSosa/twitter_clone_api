12f680598965139b49a06eee9793224efddb0814
1986SebastianSosa <104211986+1986SebastianSosa@users.noreply.github.com>
2023-01-18 10:47:07 -0300

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://twitter-clone-api-three.vercel.app/",
+];
 
 module.exports = allowedOrigins;
