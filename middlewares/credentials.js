407fe9c8b121675aea2eca7f296cf788f9eacf60
Sebastian Sosa <104211986+1986SebastianSosa@users.noreply.github.com>
2025-08-13 15:59:30 -0300

    Update credentials.js

--- middlewares/credentials.js
+++ middlewares/credentials.js
@@ -2,6 +2,7 @@ const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
+  console.log(origin);
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", "true");
